b820ba49f9075e119f8589e66af2187a09cc7bbc
genevievemacabio <89491858+genevievemacabio@users.noreply.github.com>
2024-03-02 13:25:43 +0000

    first commit

---
+++ ERC20/scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const lock = await ethers.deployContract("GN", ["0xA646C53BaB17b9a905ce1f7f6B61427547A790Eb"]);
+
+  await lock.waitForDeployment();
+
+  console.log(
+    `Token deployed to ${lock.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
